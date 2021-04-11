73f41dfe3a39c04100f834c719d439f3b0c8f7b2
Banthita Chanket <chanket_b@silpakorn.edu>
2021-04-11 20:06:27 +0700

    complete

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,6 +1,48 @@
-<form action="/save" method="post">
-    @csrf
-    <input type="text" name="long_url" placeholder="Past Long URL">
-    <button type="submit">Create Short URL</button>
 
-</form>
+
+
+
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" />
+ <style media="screen">
+     .box{
+
+         width: 1000px;
+         margin: 0 auto;
+         border: 1px solid #ccc;
+         background-color: lightblue;
+     }
+     </style>
+<body>
+<br>
+<br>
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+        <div class="container box">
+            <div class="card-header">
+                <form action="/save" method="post">
+                    @csrf
+                    <div class="input-group mb-3">
+                        <input type="text" name="long_url" placeholder="Past Long URL" class="form-control">
+
+                        <button type="submit">Create Short URL</button>
+                    </div>
+
+                </form>
+            </div>
+    </div>
+
+</body>
+
+
